9f2bedda5f40a875ccab9d2faa4fa586bb65baeb
Avichal <avichal2018lko@gmail.com>
2026-06-26 11:24:33 +0000

    init commit: basic container namespaces

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/avichal-08/gobox
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: gobox [build|run|child] [args]")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "build":
+		fmt.Println("--> Build command triggered")
+	case "run":
+		run()
+	case "child":
+		child()
+	default:
+		fmt.Printf("Unknown command: %s\n", os.Args[1])
+	}
+}
+
+func run() {
+	fmt.Printf("Running %v as parent PID %d\n", os.Args[2:], os.Getpid())
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
+	}
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("ERROR running child:", err)
+		os.Exit(1)
+	}
+}
+
+func child() {
+	fmt.Printf("Running %v as child PID %d\n", os.Args[2:], os.Getpid())
+
+	syscall.Sethostname([]byte("gobox-container"))
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("ERROR running user command:", err)
+		os.Exit(1)
+	}
+}
