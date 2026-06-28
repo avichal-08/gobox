0695b4b66f24f60948d7ea42083cdb36fdec1879
Avichal <avichal2018lko@gmail.com>
2026-06-28 12:42:52 +0000

    feat: add image pull from docker hub

--- main.go
+++ main.go
@@ -16,6 +16,12 @@ func main() {
 		run()
 	case "child":
 		child()
+	case "pull":
+		if len(os.Args) < 3 {
+			fmt.Println("Please specify an image to pull")
+			os.Exit(1)
+		}
+		pullImage(os.Args[2])
 	default:
 		fmt.Printf("Unknown command: %s\n", os.Args[1])
 		os.Exit(1)
